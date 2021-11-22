ef9660511f392b65b0e5317d6fef090d797c68df
leila <leila.kheddouma@gmail.com>
2021-11-22 18:02:45 +0100

    FuturisticLoader

---
+++ script.js
@@ -0,0 +1,20 @@
+let container = document.querySelector(".container");
+	let ring = document.querySelector(".ring-frame");
+	let disc = document.querySelector(".disc-frame");
+	let i = 1;
+	let num = 20;
+	let radius = 15;
+	let lim = 4;
+	
+	for (i; i < lim; i++){
+		let span = document.createElement('span');
+		let disk = document.createElement('span')
+		span.setAttribute("class", "ring");
+		disk.setAttribute("class", "disc");	
+		span.style.height = `${(i*20) + num}px`;
+	    span.style.width = `${(i*20) + num}px`;
+		disk.style.animationDelay = `${i-.8}s`;
+		ring.append(span);
+		disc.append(disk);
+	}
+    
